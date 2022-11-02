3e957ad2e4cb59b17a59894511a19f296f15bfb4
jaeger2801 <juan.lopez48@u.icesi.edu.co>
2022-11-02 06:57:35 -0500

    Crea intento carga productos indivualess

--- single_product.js
+++ single_product.js
@@ -2,13 +2,29 @@ let singleProduct;
 
 const link = window.location.search;
 const buscarPagina = new URLSearchParams(link);
-productLink = buscarPagina.get("id").replace('"', "");
+singleProduct = buscarPagina.get("id").replace('"', "");
 
-const productoPorSeparado = productos.find((object) => object.id == productLink)
+const productoPorSeparado = productsList.find((object) => object.id == singleProduct)
 
 let productosLink = document.getElementById("productos")
 
-productosLink.innerHTML += `
+productosLink.innerHTML += 
+
+`<section class="card_list">
+    <figure class= "card_figure"><img class= "card_img" src="${productoPorSeparado.imagenProducto}"></figure>
+    <article class="card_article">
+        <h2 class="name_detail">${productoPorSeparado.item}</h2>
+        <p class='descri_detail'>${productoPorSeparado.sobreModelo}</p>
+        <h5 class="price_detail"> ${productoPorSeparado.precio}</h5>
+        <p class="collect_detail">Collection: ${productoPorSeparado.tipo}</p>
+        
+        <button id="añadir" class="button is-black button_añadir">Añadir</button>
+
+    </article>
+</section>;`
+
+
+/* `
 
 <section class="visualizar_producto">
     <figure class="seccion_imagen_producto">
@@ -21,6 +37,6 @@ productosLink.innerHTML += `
         <span class="texto_precio">${productoPorSeparado.precio}</span>
         <span class="texto_tipo_prenda">${productoPorSeparado.tipo}</span>
     </article>
-</section>
+</section>;
 
-`
+` */
