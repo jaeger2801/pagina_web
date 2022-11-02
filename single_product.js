35d922f64762d49240934ab5aea548b77c513006
jaeger2801 <juan.lopez48@u.icesi.edu.co>
2022-11-02 05:36:06 -0500

    crea js con config para productos por separado

--- single_product.js
+++ single_product.js
@@ -10,6 +10,17 @@ let productosLink = document.getElementById("productos")
 
 productosLink.innerHTML += `
 
+<section class="visualizar_producto">
+    <figure class="seccion_imagen_producto">
+        <img class="imagen_producto" src="${productoPorSeparado.imagenProducto}">
+    </figure>
 
+    <article class="seccion_informacion">
+        <span class="titulo_producto">${productoPorSeparado.item}</span>
+        <span class="texto_info_modelo">${productoPorSeparado.sobreModelo}</span>
+        <span class="texto_precio">${productoPorSeparado.precio}</span>
+        <span class="texto_tipo_prenda">${productoPorSeparado.tipo}</span>
+    </article>
+</section>
 
 `
