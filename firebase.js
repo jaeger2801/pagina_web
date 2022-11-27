9905b8d128f9fc35a45fac2b5f094bb15a754779
jaeger2801 <juan.lopez48@u.icesi.edu.co>
2022-11-26 21:48:52 -0500

    Vincula FB con el codigo

--- firebase.js
+++ firebase.js
@@ -1,19 +1,20 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCdrfTdZQ5cDQS-RIBwkp9CYUnSEEVgNBI",
-  authDomain: "fb-proyecto-web2022.firebaseapp.com",
-  projectId: "fb-proyecto-web2022",
-  storageBucket: "fb-proyecto-web2022.appspot.com",
-  messagingSenderId: "950773677342",
-  appId: "1:950773677342:web:b02af14edc73c5c9469d0f"
-};
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCdrfTdZQ5cDQS-RIBwkp9CYUnSEEVgNBI",
+    authDomain: "fb-proyecto-web2022.firebaseapp.com",
+    projectId: "fb-proyecto-web2022",
+    storageBucket: "fb-proyecto-web2022.appspot.com",
+    messagingSenderId: "950773677342",
+    appId: "1:950773677342:web:b02af14edc73c5c9469d0f"
+  };
 
-console.log('Hola a todos');
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  console.log('Corre FB');
