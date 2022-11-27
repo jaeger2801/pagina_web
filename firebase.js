c6d659bbc795f9389ab9c975198449fb8c42a1bb
jaeger2801 <juan.lopez48@u.icesi.edu.co>
2022-11-27 13:02:21 -0500

    Crea ajustes para pantalla unicamente login

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-  import { getFirestore } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js"
+  import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js"
   
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,6 +20,19 @@
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
 
+  console.log(db);
 
+async function AddTask(){
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      first: "Ada",
+      last: "Lovelace",
+      born: 1815
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
 
-  console.log('Corre FB');
+  
