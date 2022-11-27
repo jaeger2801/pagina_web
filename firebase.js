2ffb909120b81b84233ce6beef8e98ece2e68cec
jaeger2801 <juan.lopez48@u.icesi.edu.co>
2022-11-26 21:32:08 -0500

    Crea primeras configuraciones para FireBase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCdrfTdZQ5cDQS-RIBwkp9CYUnSEEVgNBI",
+  authDomain: "fb-proyecto-web2022.firebaseapp.com",
+  projectId: "fb-proyecto-web2022",
+  storageBucket: "fb-proyecto-web2022.appspot.com",
+  messagingSenderId: "950773677342",
+  appId: "1:950773677342:web:b02af14edc73c5c9469d0f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
