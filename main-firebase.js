ff44ba6cb1605c93549fe03e4e9dcbe10c95e352
jaeger2801 <juan.lopez48@u.icesi.edu.co>
2022-11-27 20:42:50 -0500

    Commit antes de posiblemente dañar el codigo

--- main-firebase.js
+++ main-firebase.js
@@ -9,7 +9,7 @@ registerButton.addEventListener('click', (e) =>  clickFormRegister(e))
 
 function clickFormLogin(e){
     e.preventDefault()
-    console.log('prevent event');
+    
     const username = document.getElementById('loginNameTittle').value
     const password = document.getElementById('loginPasswordTittle').value
 
@@ -18,10 +18,10 @@ function clickFormLogin(e){
 
 function clickFormRegister(e){
     e.preventDefault()
-    console.log('hola?');
+    
     const nombre = document.getElementById('registerNameTittle').value
     const correo = document.getElementById('registerPasswordTittle').value
     const contraseña = document.getElementById('registerEmailTittle').value
 
-    //AddTaskRegister(nombre, correo, contraseña);
+    AddTaskRegister(nombre, correo, contraseña);
 }
